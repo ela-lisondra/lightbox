75f0f0642931abc62b8bdeabf2d19c4463c20b35
ela_lisondra <emrlisondra@gmail.com>
2021-07-22 23:14:17 +0100

    added modal.js and created modal functions

---
+++ modal.js
@@ -0,0 +1,9 @@
+$('.photos a').on('click', function(){
+    $('.modal').fadeIn(500)
+    return false
+})
+
+$('.modal-close, .modal-background').on('click', function() {
+    $('.modal').fadeOut(500)
+    return false
+})
